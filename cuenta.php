31e67869247d8f29e46e18e00013fb0bb3e8e3a2
Jimena Pardo <jimenapardo@outlook.com>
2020-05-18 21:04:13 -0300

    cuenta

--- cuenta.php
+++ cuenta.php
@@ -36,30 +36,8 @@
             <div class="collapse navbar-collapse " id="navbarSupportedContent">
                 <ul class="navbar-nav mr-auto text-center">
                     <li class="nav-item "> <a class="nav-link" href="home.php">Inicio </a> </li>
-                    <li class="nav-item"> <a class="nav-link" href="#">Más recientes</a> </li>
-                    <li class="nav-item active dropdown "> <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false"> Géneros </a>
-                        <div class="dropdown-menu text-center " aria-labelledby="navbarDropdown">
-                            <a class="dropdown-item active" href="#">Todos</a>
-                            <div class="dropdown-divider"></div>
-                            <a class="dropdown-item" href="#">Acción</a>
-                            <a class="dropdown-item" href="#">Argentina</a>
-                            <a class="dropdown-item" href="#">Aventura</a>
-                            <a class="dropdown-item" href="#">Ciencia Ficción</a>
-                            <a class="dropdown-item" href="#">Comedia</a>
-                            <a class="dropdown-item" href="#">Drama</a>
-                            <a class="dropdown-item" href="#">Infantiles</a>
-                            <a class="dropdown-item" href="#">Misterio</a>
-                            <a class="dropdown-item" href="#">Policiales</a>
-                            <a class="dropdown-item" href="#">Romance</a>
-                            <a class="dropdown-item" href="#">Terror</a>
-                            <a class="dropdown-item" href="#">Thrillers</a>
-                        </div>
-                        <li class="nav-item"> <a class="nav-link" href="#">Mi lista</a> </li>
-                    </li>
-                </ul>
-
-
-
+                    </ul>
+                   
                 <form class="form-inline my-2 my-lg-0"> <input class="form-control mr-sm-2 " type="search" placeholder="Buscar..." aria-label="Search"> <button class="btn btn-outline-danger my-2 my-sm-0" type="submit">
                 <svg class="bi bi-search" width="1.4em" height="1.3em" viewBox="0 0 16 16" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
                 <path fill-rule="evenodd" d="M10.442 10.442a1 1 0 011.415 0l3.85 3.85a1 1 0 01-1.414 1.415l-3.85-3.85a1 1 0 010-1.415z" clip-rule="evenodd"/>
@@ -81,7 +59,7 @@
                             <path fill-rule="evenodd" d="M9.928 1.723c-.243-.97-1.62-.97-1.863 0l-.072.286a.96.96 0 01-1.622.435l-.204-.212c-.695-.718-1.889-.03-1.614.932l.08.283a.96.96 0 01-1.186 1.187l-.283-.081c-.961-.275-1.65.919-.932 1.614l.212.204a.96.96 0 01-.435 1.622l-.286.072c-.97.242-.97 1.62 0 1.863l.286.071a.96.96 0 01.435 1.622l-.212.205c-.718.695-.03 1.888.932 1.613l.283-.08a.96.96 0 011.187 1.187l-.081.283c-.275.96.919 1.65 1.614.931l.204-.211a.96.96 0 011.622.434l.072.286c.242.97 1.62.97 1.863 0l.071-.286a.96.96 0 011.622-.434l.205.212c.695.718 1.888.029 1.613-.932l-.08-.283a.96.96 0 011.187-1.188l.283.081c.96.275 1.65-.918.931-1.613l-.211-.205A.96.96 0 0115.983 10l.286-.071c.97-.243.97-1.62 0-1.863l-.286-.072a.96.96 0 01-.434-1.622l.212-.204c.718-.695.029-1.889-.932-1.614l-.283.08a.96.96 0 01-1.188-1.186l.081-.283c.275-.961-.918-1.65-1.613-.932l-.205.212A.96.96 0 0110 2.009l-.071-.286zm-.932 12.27a4.998 4.998 0 100-9.994 4.998 4.998 0 000 9.995z" clip-rule="evenodd"/>
                           </svg> <b class="caret"></b></a>
                         <div class="dropdown-menu dropdown-menu-right text-right" aria-labelledby="navbarDropdown">
-                            <a class="dropdown-item" href="#">Administrar perfiles</a>
+                            <a class="dropdown-item" href="seleccionarPerfil.php">Administrar perfiles</a>
                             <div class="dropdown-divider"></div>
                             <a class="dropdown-item" href="#">Cuenta</a>
                             <a class="dropdown-item" href="preguntasfrecuentes.php">Preguntas Frecuentes</a>
