b003a53c56562468a007155d102cdea150e024d9
Jimena Pardo <jimenapardo@outlook.com>
2020-05-20 09:42:15 -0300

    nuevo home

--- home.php
+++ home.php
@@ -6,23 +6,20 @@
 	$sesion = new manejadorSesiones;
 ?>
 
-<!DOCTYPE html>
-<html lang="en">
 
+<!DOCTYPE html>
+<html>
 <head>
-    <meta charset="utf-8">
     <title>Bookflix</title>
     <link rel="icon" href="img/logo2.png">
-    <meta name="viewport" content="width=device-width, initial-scale=1, minimum-scale=1, maximum-scale=1">
+	<meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.1.0/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.0/umd/popper.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.1.0/js/bootstrap.min.js"></script>
 
-    <!-- Link Swiper's CSS -->
-    <link rel="stylesheet" href="package/css/swiper.min.css">
-    <link rel="stylesheet" href="css/styles.css">
-
-    <!-- Bootstrap CSS -->
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
-
-    <?php     
+  <?php     
     if (isset($_SESSION['PERMISO'])) {   
    
     switch ($_SESSION['PERMISO']) {
@@ -39,7 +36,7 @@
                     <div class="collapse navbar-collapse " id="navbarSupportedContent">
                         <ul class="navbar-nav mr-auto text-center">
                             <li class="nav-item active"> <a class="nav-link" href="home.php">Inicio </a> </li>
-                            <li class="nav-item"> <a class="nav-link" href="#">Novedades</a> </li>
+                            <li class="nav-item"> <a class="nav-link" href="#">Más recientes</a> </li>
                             <li class="nav-item dropdown "> <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false"> Géneros </a>
                                 <div class="dropdown-menu text-center " aria-labelledby="navbarDropdown">
                                     <a class="dropdown-item" href="generos.php">Todos</a>
@@ -110,7 +107,7 @@
                         <div class="collapse navbar-collapse " id="navbarSupportedContent">
                             <ul class="navbar-nav mr-auto text-center">
                                 <li class="nav-item active"> <a class="nav-link" href="home.php">Inicio </a> </li>
-                                <li class="nav-item"> <a class="nav-link" href="#">Novedades</a> </li>
+                                <li class="nav-item"> <a class="nav-link" href="#">Más recientes</a> </li>
                                 <li class="nav-item dropdown "> <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false"> Géneros </a>
                                     <div class="dropdown-menu text-center " aria-labelledby="navbarDropdown">
                                         <a class="dropdown-item" href="generos.php">Todos</a>
@@ -142,13 +139,13 @@
 
                                 <li class="nav-item ">
                                     <a class="nav-link mr-2" href="#">
-                                        <svg class="bi bi-bell-fill" width="1.5em" height="2em" viewBox="0 0 16 16" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
+                                        <svg class="bi bi-bell-fill" width="1.6em" height="2em" viewBox="0 0 16 16" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
                                     <path d="M8 16a2 2 0 002-2H6a2 2 0 002 2zm.995-14.901a1 1 0 10-1.99 0A5.002 5.002 0 003 6c0 1.098-.5 6-2 7h14c-1.5-1-2-5.902-2-7 0-2.42-1.72-4.44-4.005-4.901z"/> </svg></a>
                                 </li>
 
                                 <li class="nav-item dropdown " style="display: inline-block;">
                                     <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                                        <svg class="bi bi-gear-wide" width="1.5em" height="1.3em" viewBox="0 0 16 16" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
+                                    <svg class="bi bi-gear-wide" width="1.5em" height="1.3em" viewBox="0 0 16 16" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
                                         <path fill-rule="evenodd" d="M9.928 1.723c-.243-.97-1.62-.97-1.863 0l-.072.286a.96.96 0 01-1.622.435l-.204-.212c-.695-.718-1.889-.03-1.614.932l.08.283a.96.96 0 01-1.186 1.187l-.283-.081c-.961-.275-1.65.919-.932 1.614l.212.204a.96.96 0 01-.435 1.622l-.286.072c-.97.242-.97 1.62 0 1.863l.286.071a.96.96 0 01.435 1.622l-.212.205c-.718.695-.03 1.888.932 1.613l.283-.08a.96.96 0 011.187 1.187l-.081.283c-.275.96.919 1.65 1.614.931l.204-.211a.96.96 0 011.622.434l.072.286c.242.97 1.62.97 1.863 0l.071-.286a.96.96 0 011.622-.434l.205.212c.695.718 1.888.029 1.613-.932l-.08-.283a.96.96 0 011.187-1.188l.283.081c.96.275 1.65-.918.931-1.613l-.211-.205A.96.96 0 0115.983 10l.286-.071c.97-.243.97-1.62 0-1.863l-.286-.072a.96.96 0 01-.434-1.622l.212-.204c.718-.695.029-1.889-.932-1.614l-.283.08a.96.96 0 01-1.188-1.186l.081-.283c.275-.961-.918-1.65-1.613-.932l-.205.212A.96.96 0 0110 2.009l-.071-.286zm-.932 12.27a4.998 4.998 0 100-9.994 4.998 4.998 0 000 9.995z" clip-rule="evenodd"/>
                                     </svg> <b class="caret"></b></a>
                                     <div class="dropdown-menu dropdown-menu-right text-right" aria-labelledby="navbarDropdown">
@@ -219,131 +216,122 @@
  <?php
 }
  ?>
+
 </head>
+<body style="background-color: #221f1f; padding-top: 6%;">
+<h3 style="color:#f1f1f5"> &nbsp &nbsp NOVEDADES</h3>
+<div class="container-fluid">
+	<div class="row">
+		<div class="col-sm-12">
+			<div id="inam" class="carousel slide" data-ride="carousel">
+				<div class="carousel-inner">
+                    <?php 
+                        $sql="SELECT idLibro, nombreLibro, descripcionLibro, portadaLibro FROM libro INNER JOIN novedadlibro ON (libro.isbn = novedadlibro.isbn)"; 
+                        $query= mysqli_query($conexion,$sql);                
+                        while ($name = mysqli_fetch_array($query)) {
+                            var_dump($name);
+                    ?>
+					<div class="carousel-item ">
+						<div class="container">
+						 	<div class="row">
+						 		<div class="col-sm-12 col-lg-4">
+						 			<div class="card" style="width: 300px;margin: auto;">
+						 				<img src="<?php echo $name["portadaLibro"]?>" class="card-img-top">
+						 				<div class="card-body">
+						 					<h4 class="card-title"><?php echo $name["nombreLibro"]?></h4>
+						 					<p class="card-text"><?php echo $name["descripcionLibro"]?></p>
+                                            <a> <button type="button" class="btn btn-danger">Leer</button></a>
+                                            <a> <button type="button" class="btn btn-danger">Agregar a Mi lista</button></a> 					
+						 				</div>
+						 			</div>
+						 		</div>
+						 		<!-- <div class="col-sm-12 col-lg-4">
+						 			<div class="card" style="width: 300px;">
+						 				<img src="<?php echo $name["portadaLibro"]?>" class="card-img-top">
+						 				<div class="card-body">
+						 					<h4 class="card-title"><?php echo $name["nombreLibro"]?></h4>
+						 					<p class="card-text"><?php echo $name["descripcionLibro"]?></p>
+                                             <a> <button type="button" class="btn btn-danger">Leer</button></a>
+                                             <a> <button type="button" class="btn btn-danger">Agregar a Mi lista</button></a>
+						 				</div>
+						 			</div>
+						 		</div>
+						 		<div class="col-sm-12 col-lg-4">
+						 			<div class="card" style="width: 300px;">
+						 				<img src="<?php echo $name["portadaLibro"]?>" class="card-img-top">
+						 				<div class="card-body">
+						 					<h4 class="card-title"><?php echo $name["nombreLibro"]?></h4></h4>
+						 					<p class="card-text"><?php echo $name["descripcionLibro"]?></p>
+                                             <a> <button type="button" class="btn btn-danger">Leer</button></a>
+                                             <a> <button type="button" class="btn btn-danger">Agregar a Mi lista</button></a>
+						 				</div>
+						 			</div>
+						 		</div>
+						 	</div>
+						</div>
+					</div><!--fin carousel-item active--> -->
+					<!-- <div class="carousel-item">
+						<div class="container">
+						 	<div class="row">
+						 		<div class="col-sm-12 col-lg-4">
+						 			<div class="card" style="width: 300px;margin: auto;">
+						 				<img src="img/3.jpg" class="card-img-top">
+						 				<div class="card-body">
+						 					<h4 class="card-title">Libro Z</h4>
+						 					<p class="card-text">Skin masks help us to make are skin fresh and also they protect our skin from the harm rays of sun</p>
+                                             <a> <button type="button" class="btn btn-danger">Leer</button></a>
+                                             <a> <button type="button" class="btn btn-danger">Agregar a Mi lista</button></a>
+						 				</div>
+						 			</div>
+						 		</div>
+						 		<div class="col-sm-12 col-lg-4">
+						 			<div class="card" style="width: 300px;">
+						 				<img src="img/4.jpg" class="card-img-top">
+						 				<div class="card-body">
+						 					<h4 class="card-title">Libro M</h4>
+						 					<p class="card-text">Skin masks help us to make are skin fresh and also they protect our skin from the harm rays of sun</p>
+                                             <a> <button type="button" class="btn btn-danger">Leer</button></a>
+                                             <a> <button type="button" class="btn btn-danger">Agregar a Mi lista</button></a>
+						 				</div>
+						 			</div>
+						 		</div>
+						 		<div class="col-sm-12 col-lg-4">
+						 			<div class="card" style="width: 300px;">
+						 				<img src="img/10.jpg" class="card-img-top">
+						 				<div class="card-body">
+						 					<h4 class="card-title">Libro N</h4>
+						 					<p class="card-text">Skin masks help us to make are skin fresh and also they protect our skin from the harm rays of sun</p>
+                                             <a> <button type="button" class="btn btn-danger">Leer</button></a>
+                                             <a> <button type="button" class="btn btn-danger">Agregar a Mi lista</button></a>
+						 				</div>						 				
+						 			</div>						 			
+						 		</div>						 		
+						 	</div>						 	
+						</div>						
+					</div> carousel-item fin -->
+					
+				</div> <!-- carousel-inner fin-->
+				<a href="#inam" class="carousel-control-prev" data-slide="prev">
+					<span class="carousel-control-prev-icon"></span>
+				</a>
+				<a href="#inam" class="carousel-control-next" data-slide="next">
+					<span class="carousel-control-next-icon"></span>
+				</a>
+			</div> <!-- fin inam-->			
+		</div> <!-- fin col-sm 12-->	
+	</div> <!-- fin row-->	
+</div> <!-- fin container-fluid-->	
 
-<body style="background-color: #221f1f;">
-
- <!--las novedades las ven todos, registrados, no registrados y administrador
-        deberia haber un carrusel fijo para las novedades-->
-        <div class="netflix-slider mx-5">
-                    <h2 class="titulo">Novedades </h2>
-                    <div class="swiper-container">
-                        <div class="swiper-wrapper">
-                            <?php  
-                                for ($i=0; $i < 7 ; $i++) { 
-                            ?>  
-                            <div class="swiper-slide"><img src="img/10.jpg" alt="foto"></div><?php 
-                            } ?>
-                        </div>
-                        <!-- Add Pagination -->
-                        <!-- <div class="swiper-pagination"></div> -->
-                        <div class="swiper-button-next"></div>
-                        <div class="swiper-button-prev"></div>
-                    </div>
-                </div>
-            
-    <?php 
-        if (isset($_SESSION['PERMISO'])) {
-            
-        
-          if (($_SESSION['PERMISO'] == 1) || ($_SESSION['PERMISO'] == 2)){
-              /* si es basico o premium ademas deberia ver las sugerencias, el administrador no */?>
-                 <div class="netflix-slider mx-5">
-                        <h2 class="titulo">Sugerencias </h2>
-                        <div class="swiper-container">
-                            <div class="swiper-wrapper">
-                                <?php  
-                                    for ($i=0; $i < 7 ; $i++) { 
-                                ?>  
-                                <div class="swiper-slide"><img src="img/6.jpg" alt="foto"></div><?php 
-                                } ?>
-                            </div>
-                            <!-- Add Pagination -->
-                            <!-- <div class="swiper-pagination"></div> -->
-                            <div class="swiper-button-next"></div>
-                            <div class="swiper-button-prev"></div>
-                        </div>
-                    </div>
-     <?php 
-         }
-         }
-     ?>
-
-                
-    <?php
-        if(isset($_SESSION['PERMISO'])){
-        if (($_SESSION['PERMISO'] == 1) || ($_SESSION['PERMISO'] == 2) || ($_SESSION['PERMISO'] == 3)){
-            /* cualquier usuario registrado, sea basico/premium/administrador puede ver novedades + generos*/ ?>
-            <?php
-
-                $sql="SELECT nombreGenero FROM genero"; // no se como seguir para que le quede la cantidad de libros para despues recorrer 
-                $sql2= "SELECT *
-                        FROM libro  
-                            INNER JOIN genero  ON (libro.idGenero = genero.idGenero) 
-                        WHERE libro.idGenero= genero.idGenero";
-                $query= mysqli_query($conexion,$sql);
-                $query2= mysqli_query($conexion,$sql2);
-                while ($name = mysqli_fetch_array($query)) {
-                ?>
-                <!--primer slide de recomendados -->
-                <div class="netflix-slider mx-5">
-                    <h2 class="titulo"><?php echo $name["nombreGenero"]?> </h2>
-                    <div class="swiper-container">
-                        <div class="swiper-wrapper">
-                            <?php  
-                                for ($i=0; $i < mysqli_num_rows($query2) ; $i++) { 
-                            ?>  
-                            <div class="swiper-slide"><img src="img/1.jpg" alt="X-Men"></div><?php 
-                            } ?>
-                        </div>
-                        <!-- Add Pagination -->
-                        <!-- <div class="swiper-pagination"></div> -->
-                        <div class="swiper-button-next"></div>
-                        <div class="swiper-button-prev"></div>
-                    </div>
-                </div>
-            
-                <?php
+<?php
                     } 
                 ?>
-        
-       <?php 
-     }
-     }
-
-       ?>
 
 
 
-      
-
-    <!-- Swiper -->
-    
-    <!-- Swiper JS -->
-    <script src="package/js/swiper.min.js"></script>
-
-    <!-- Initialize Swiper -->
-    <script>
-        var swiper = new Swiper('.swiper-container', {
-            slidesPerView: 6,
-            spaceBetween: 10,
-            slidesPerGroup: 2,
-            pagination: {
-                el: '.swiper-pagination',
-                clickable: true,
-            },
-            navigation: {
-                nextEl: '.swiper-button-next',
-                prevEl: '.swiper-button-prev',
-            },
-        });
-    </script>
-
-
-    <!-- Logout Modal-->
-    <div class="modal fade" id="logoutModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true" >
+ <!-- Logout Modal-->
+ <div class="modal fade" id="logoutModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true" >
             <div class="modal-dialog" role="document">
-                <div class="modal-content" style="background-color: #221f1f;">
+                <div class="modal-content" >
                     <div class="modal-header">
                         <h5 class="modal-title" id="exampleModalLabel">¿Estás seguro que querés cerrar la sesión?</h5>
                         <button class="close" type="button" data-dismiss="modal" aria-label="Close">
@@ -359,19 +347,14 @@
             </div>
         </div>
 
-
-    <!--Scripts de bootstrap -->
-    <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js " integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n " crossorigin="anonymous "></script>
-    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js " integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo " crossorigin="anonymous "></script>
-    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js " integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6 " crossorigin="anonymous "></script>
-    <!-- pie de pagina -->
-    <br><br>
+ <!-- pie de pagina -->
+ <br><br>
     <hr width="92.5% " color="gray ">
-    <footer>
-    <a class="pfrecuentes" href="preguntasfrecuentes.php"><u>Preguntas Frecuentes</u></a>
+    <footer style="margin: 20px auto 0;  padding: 0 4%;  display: flex; -webkit-box-direction: normal; text-align: center;">
+    <a class="pfrecuentes" href="preguntasfrecuentes.php" style="margin: 0px 0px 14px 0px; padding: 0px; font-size: 13px; display: flex; color: #808080;
+    text-align: center;"><u>Preguntas Frecuentes</u></a>
         <hr>
         <hr>
     </footer>
 </body>
-
 </html>
