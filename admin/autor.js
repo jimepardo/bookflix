a5707a076608183740389661d6bf4c2c75a1aaa7
Jimena Pardo <jimenapardo@outlook.com>
2020-05-31 06:04:50 -0300

    tablas

---
+++ admin/autor.js
@@ -0,0 +1,107 @@
+$(document).ready(function(){
+var id, opcion;
+opcion = 4;
+tablaAutor = $('#tablaAutor').DataTable({ 
+    "ajax":{            
+        "url": "vistas/crudautor.php", 
+        "method": 'POST', //usamos el metodo POST
+        "data":{opcion:opcion}, //enviamos opcion 4 para que haga un SELECT
+        "dataSrc":""
+    },
+    "columns":[
+        {"data": "idAutor"},
+        {"data": "nombreAutor"},
+        {"data": "borradoLogico"},
+        {"data": "borradoParanoagregar"},
+        {"defaultContent": "<div class='text-center'><div class='btn-group'><button class='btn btn-secondary btn-sm btnEditar'><i class='material-icons'>Modificar</i></button><button class='btn btn-danger btn-sm btnBorrar'><i class='material-icons'>Borrar</i></button></div></div>"
+    }],
+     
+        //Para cambiar el lenguaje a español
+    "language": {
+            "lengthMenu": "Mostrar _MENU_ registros",
+            "zeroRecords": "No se encontraron resultados",
+            "info": "Mostrando registros del _START_ al _END_ de un total de _TOTAL_ registros",
+            "infoEmpty": "Mostrando registros del 0 al 0 de un total de 0 registros",
+            "infoFiltered": "(filtrado de un total de _MAX_ registros)",
+            "sSearch": "Buscar:",
+            "oPaginate": {
+                "sFirst": "Primero",
+                "sLast":"Último",
+                "sNext":"Siguiente",
+                "sPrevious": "Anterior"
+             },
+             "sProcessing":"Procesando...",
+    },
+});
+
+var fila; //capturar la fila para editar o borrar el registro
+
+$("#formAutor").submit(function(e){
+    e.preventDefault();    
+    nombre = $.trim($("#nombre").val());
+    borrado =$.trim($("#borrado").val());
+    borrado2 =$.trim($("#borrado2").val());  
+    $.ajax({
+        url: "vistas/crudautor.php",
+        type: "POST",
+        dataType: "json",
+        data: {nombre:nombre, borrado:borrado, borrado2:borrado2, id:id, opcion:opcion},
+        success: function(data){    
+            tablaAutor.ajax.reload(null,false);
+            document.getElementById("nombre").disabled = false;
+            document.getElementById("borrado").disabled = false;
+            document.getElementById("borrado2").disabled = false;
+        }        
+    });
+    $('#modalCRUD').modal('hide');     
+}); 
+    
+$("#btnNuevo").click(function(){
+    opcion = 1; //alta
+    id=null;
+    document.getElementById("borrado").disabled = true;
+    document.getElementById("borrado2").disabled = true;
+    $("#formAutor").trigger("reset");
+    $(".modal-header").css("background-color", "#CE0909");
+    $(".modal-header").css("color", "#F5F5F1");
+    $(".modal-title").text("Nuevo Autor");            
+    $('#modalCRUD').modal('show');         
+});    
+    
+//botón EDITAR    
+$(document).on("click", ".btnEditar", function(){
+    opcion = 2; //editar
+    fila = $(this).closest("tr");
+    id = parseInt(fila.find('td:eq(0)').text());
+    nombre = fila.find('td:eq(1)').text();
+    
+    $("#nombre").val(nombre);
+    
+    $(".modal-header").css("background-color", "#7D7A7A");
+    $(".modal-header").css("color", "#F5F5F1");
+    $(".modal-title").text("Modificar autor");            
+    $('#modalCRUD').modal('show');  
+
+    document.getElementById("borrado").disabled = true;
+    document.getElementById("borrado2").disabled = true;    
+});
+
+//botón BORRAR
+$(document).on("click", ".btnBorrar", function(){    
+    opcion = 3; //borrar
+    fila = $(this).closest("tr");
+    id = parseInt(fila.find('td:eq(0)').text());
+    borrado = fila.find('td:eq(2)').text();
+    borrado2 = fila.find('td:eq(3)').text();
+    
+    $("#borrado").val(borrado);
+    $("#borrado2").val(borrado2);
+
+    $(".modal-header").css("background-color", "#CE0909");
+    $(".modal-header").css("color", "#F5F5F1");
+    $(".modal-title").text("Borrar autor");            
+    $('#modalCRUD').modal('show'); 
+    document.getElementById("nombre").disabled = true;
+});
+
+});
