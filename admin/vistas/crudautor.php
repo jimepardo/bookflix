f3a9ee2a5dcf8a4cc3d03f8807bc5176c5100f30
Jimena Pardo <jimenapardo@outlook.com>
2020-05-31 05:57:15 -0300

    crud

---
+++ admin/vistas/crudautor.php
@@ -0,0 +1,54 @@
+<?php
+include_once 'conexion.php';
+$objeto = new Conexion();
+$conexion = $objeto->Conectar();
+
+// Recepción de los datos enviados mediante POST desde el JS   
+
+$nombre     = (isset($_POST['nombre'])) ? $_POST['nombre'] : '';
+$borrado    = (isset($_POST['borrado'])) ? $_POST['borrado'] : '';
+$borrado2   = (isset($_POST['borrado2'])) ? $_POST['borrado2'] : '';
+$opcion     = (isset($_POST['opcion'])) ? $_POST['opcion'] : '';
+$id         = (isset($_POST['id'])) ? $_POST['id'] : '';
+
+switch($opcion){
+    case 1: //alta
+        $consulta = "INSERT INTO autor (nombreAutor) VALUES('$nombre') ";			
+        $resultado = $conexion->prepare($consulta);
+        $resultado->execute(); 
+
+        $consulta = "SELECT * FROM autor ORDER BY idAutor DESC LIMIT 1";
+        $resultado = $conexion->prepare($consulta);
+        $resultado->execute();
+        $data=$resultado->fetchAll(PDO::FETCH_ASSOC);
+        break;
+    case 2: //modificación
+        $consulta = "UPDATE autor SET nombreAutor='$nombre' WHERE idAutor='$id' and borradoLogico=0 and borradoParanoagregar=0 ";		
+        $resultado = $conexion->prepare($consulta);
+        $resultado->execute();        
+        
+        $consulta = "SELECT * FROM autor WHERE idAutor='$id' ";       
+        $resultado = $conexion->prepare($consulta);
+        $resultado->execute();
+        $data=$resultado->fetchAll(PDO::FETCH_ASSOC);
+        break;        
+    case 3://baja
+        $consulta = "UPDATE autor SET borradoLogico='$borrado', borradoParanoagregar='$borrado2' WHERE idAutor='$id'";		
+        $resultado = $conexion->prepare($consulta);
+        $resultado->execute();        
+        
+        $consulta = "SELECT * FROM autor WHERE idAutor='$id' ";       
+        $resultado = $conexion->prepare($consulta);
+        $resultado->execute();
+        $data=$resultado->fetchAll(PDO::FETCH_ASSOC);                           
+        break;   
+    case 4:    
+        $consulta = "SELECT * FROM autor";
+        $resultado = $conexion->prepare($consulta);
+        $resultado->execute();        
+        $data=$resultado->fetchAll(PDO::FETCH_ASSOC);
+        break;     
+}
+
+print json_encode($data, JSON_UNESCAPED_UNICODE); //enviar el array final en formato json a JS
+$conexion = NULL;
