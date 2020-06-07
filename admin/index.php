b178e215c2504362de6bc76d2809a9a61fb2ca2e
Jimena Pardo <jimenapardo@outlook.com>
2020-06-07 00:00:48 -0300

    novedad generales

--- admin/index.php
+++ admin/index.php
@@ -161,7 +161,28 @@
       </div>
     </div>
 
- 
+  <!-- Novedades Generales-->
+  <div class="col-xl-4 col-md-6 mb-4">
+    <div class="card border-left-primary shadow h-100 py-2">
+      <div class="card-body">
+        <div class="row no-gutters align-items-center">
+          <div class="col mr-2">
+            <div class="text-xs font-weight-bold text-primary text-uppercase mb-1">Listado de novedades generales</div>
+              <a href="novedadesgenerales.php" class="btn btn-light btn-icon-split">
+                <span class="icon text-gray-600">
+                  <i class="fas fa-arrow-right"></i>
+                </span>
+                <span class="text">Carga y modificación</span>
+              </a>
+            </div>
+            <div class="col-auto">
+              <i class="fas fa-newspaper fa-2x text-gray-300"></i>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+    
  <!-- Adicional -->
  <div class="col-xl-4 col-md-6 mb-4">
     <div class="card border-left-success shadow h-100 py-2">
