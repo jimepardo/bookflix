c9474782c159a68d7f014588f14a7049e60c32b0
Jimena Pardo <jimenapardo@outlook.com>
2020-06-13 20:31:33 -0300

    algo

--- admin/vistas/parte_superior.php
+++ admin/vistas/parte_superior.php
@@ -80,7 +80,7 @@
             <!-- Nav Item - Pages Collapse Menu -->
             <li class="nav-item">
                 <a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#collapseTwo" aria-expanded="true" aria-controls="collapseTwo">
-                    <i class="fas fa-fw fa-cog"></i>
+                    <i class="fas fa-book"></i>
                     <span>Libros</span>
                 </a>
                 <div id="collapseTwo" class="collapse" aria-labelledby="headingTwo" data-parent="#accordionSidebar">
