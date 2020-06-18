bc7a824aa39a4ae10ee7912c0ab7e920f92be260
ErnestoLG <gargiulolucio@gmail.com>
2020-06-17 22:26:02 -0300

    regis

--- seleccionar.php
+++ seleccionar.php
@@ -9,11 +9,7 @@
 	$sesion->set('GENEROFAV',$_GET['GENEROFAV']);
 	$sesion->set('AUTORFAV',$_GET['AUTORFAV']);
 	$sesion->set('PERFIL',$_GET['PERFIL']);
-<<<<<<< HEAD
 	$sesion->set('IDPERFIL',$_GET['IDPERFIL']);
-=======
-
->>>>>>> 273d0fb6a45b2db5bedd6375ba7ab8b7d5dc4e59
 	header('Location: home.php');
 
 ?>
